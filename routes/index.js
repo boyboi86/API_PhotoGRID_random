3d4a4d1da4b7229e491e8e6ba4ed2f518ad02924
boyboi86 <boyboi86@gmail.com>
2016-11-06 21:43:47 +0800

    update routes with fs to rename files before upload to s3

---
+++ routes/index.js
@@ -0,0 +1,52 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const app = require('../app');
+const fs = require('fs');
+const os = require('os');
+const formidable = require('formidable');
+
+  router.get('/', (req, res, next) => {
+    res.render('index', {
+      host: app.get('host')
+    });
+  });
+/*To match index.html with an ajax post method, formidable is a low level package*/
+  router.post('/upload', (req, res, next) => {
+/*This function generateFilename randomly generates 14 alphanumeric charset
+  and string date as name thereby guarantee unqiue identifier*/
+    const generateFilename = filename => {
+      let ext_regex = /(?:\.([^.]+))?$/;
+      let ext = ext_regex.exec(filename)[1];
+      let date = new Date().getTime();
+      let charBank = "abcdefghijklmnopqrstuvwxyz";
+      let fstring = '';
+      for(let i = 0; i < 15; i++){
+        fstring += charBank[parseInt(Math.random()*26)];
+      }
+      return (fstring += date + '.' + ext);
+    }
+
+
+    let tmpFile, nfile, fname;
+    let newForm = new formidable.IncomingForm();
+    newForm.keepExtensions = true;
+    newForm.parse(req, (err, fields, files) => {
+      tmpFile = files.upload.path;
+      fname = generateFilename(files.upload.name);
+      /* This is to ensure that regardless of OS we can still have access to tmp directory*/
+      nfile = os.tmpDir() + '/' + fname;
+      res.writeHead(200, {'content-type': 'text/plain'});
+      res.end();
+    })
+
+    newForm.on('end', () => {
+      /*once upload done, fs will rename the files*/
+      fs.rename(tmpFile, nfile, () => {
+
+      })
+    })
+  })
+
+
+module.exports = router;
